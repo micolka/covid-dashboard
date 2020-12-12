ac5bbbf681801ce203809ec08f433d465b89c78f
micolka <mic_la@tut.by>
2020-12-12 23:42:31 +0300

    feat: implement api functions

---
+++ src/API/API.ts
@@ -0,0 +1,68 @@
+import {
+  TCountry, TAllStatus, TSummary, TGlobal, WorldTotalWIP,
+} from './APITypes';
+
+export const API = {
+  URL: 'https://api.covid19api.com',
+  ENDPOINTS: {
+    ALL: 'all',
+    DATE: 'date',
+    CONFIRMED: 'confirmed',
+    COUNTRIES: 'countries',
+    COUNTRY: 'country',
+    DAYONE: 'dayone',
+    LIVE: 'live',
+    SUMMARY: 'summary',
+    STATUS: 'status',
+    TOTAL: 'total',
+    WORLD: 'world',
+  },
+};
+
+export const getCountries = async (): Promise<TCountry[]> => {
+  const { URL, ENDPOINTS } = API;
+  const response = await fetch(`${URL}/${ENDPOINTS.COUNTRIES}`);
+  return (await response.json()) as TCountry[];
+};
+
+export const getSummary = async (): Promise<TSummary> => {
+  const { URL, ENDPOINTS } = API;
+  const response = await fetch(`${URL}/${ENDPOINTS.SUMMARY}`);
+  return (await response.json()) as TSummary;
+};
+
+export const getDayOneTotalAllStatus = async (name: string): Promise<TAllStatus[]> => {
+  const { URL, ENDPOINTS } = API;
+  const { DAYONE, COUNTRY, TOTAL } = ENDPOINTS;
+
+  const response = await fetch(`${URL}/${TOTAL}/${DAYONE}/${COUNTRY}/${name}`);
+  return (await response.json()) as TAllStatus[];
+};
+
+export const getByCountryAllStatus = async (
+  name: string,
+  from: string,
+  to: string
+): Promise<TAllStatus[]> => {
+  const { URL, ENDPOINTS } = API;
+  const { COUNTRY } = ENDPOINTS;
+
+  const response = await fetch(`${URL}/${COUNTRY}/${name}?from=${from}&to=${to}`);
+  return (await response.json()) as TAllStatus[];
+};
+
+export const getWorldWIP = async (from: string, to: string): Promise<TGlobal[]> => {
+  const { URL, ENDPOINTS } = API;
+  const { WORLD } = ENDPOINTS;
+
+  const response = await fetch(`${URL}/${WORLD}?from=${from}&to=${to}`);
+  return (await response.json()) as TGlobal[];
+};
+
+export const getWorldTotalWIP = async (): Promise<WorldTotalWIP> => {
+  const { URL, ENDPOINTS } = API;
+  const { WORLD, TOTAL } = ENDPOINTS;
+
+  const response = await fetch(`${URL}/${WORLD}/${TOTAL}`);
+  return (await response.json()) as WorldTotalWIP;
+};
