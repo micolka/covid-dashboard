66e099859f4713526ab325f46b763e27fdd98f52
asadovskaya <sashasadovskaya@gmail.com>
2020-12-15 23:21:22 +0300

    feat: add global deaths

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { getSummary } from '@/API/API';
 import styles from '@/assets/stylesheets/stats.scss';
 
-const Stats = () => <div className={styles['stats-wrapper']}>Statistics</div>;
+const Stats = props => {
+  const [data, setData] = useState({});
+  const [countries, setCountries] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getSummary();
+      const countries = await result.Countries[0].Country;
+      const countriesDeaths = await result.Countries[0].TotalDeaths;
+      setData(result);
+      setCountries(countries);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles['stats-wrapper']}>
+      <h2>Global Deaths</h2>
+      <p>
+        {data.Global ? data.Global.TotalDeaths : ''}
+      </p>
+    </div>
+  );
+};
 export default Stats;
