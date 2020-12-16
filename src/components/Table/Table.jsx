846f8064b6563ef245059f95c95e5b9ef644150f
Vladimir <17500175@mail.ru>
2020-12-16 18:53:26 +0300

    feat: add Table functionality

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,51 +1,77 @@
 import React, { useEffect, useState } from 'react';
 
+import { selectors } from './Constants';
 import styles from '@/assets/stylesheets/table.scss';
 
 const Table = (props) => {
 const [numSelector, setNumSelector] = useState(0);
+const [value, Setvalue] = useState('');
 
-// console.log((props.summary.Countries[0].Country))
-// console.log((props.summary.Countries.length))
-// console.log((props.summary.Countries))
-const sum = props.summary.Countries;
-console.log(props.summary.Global)
-const an = 'at';
-const selectors = ['TotalConfirmed', 'TotalDeaths', 'TotalRecovered', 'NewConfirmed', 'NewDeaths', 'NewRecovered',
-'TotalConfirmed/100K citizens', 'TotalDeaths/100K citizens', 'TotalRecovered/100K citizens',
- 'NewConfirmed/100K citizens', 'NewDeaths/100K citizens', 'NewRecovered/100K citizens'];
+let sum = props.summary.Countries;
+const population = props.summary.population;
+// console.log(population)
  const nameSelector = selectors[numSelector];
+
+// console.log(sum)
 
  const changeSelector = (e) => {
    if (e.target.closest('button').id === 'next') {
-     return setNumSelector (prev => (prev + 1) % 11)
+     return setNumSelector (prev => (prev + 1) % 12)
    } else {
      return setNumSelector (prev => {
-       if(prev <= 0) return 12 - Math.abs((prev - 1)) % 11;
-       return (prev - 1) % 11;
+       if(prev <= 0) return 12 - Math.abs((prev - 1)) % 12;
+       return (prev - 1) % 12;
       })
    }
  }
 
-return (
-<div className={styles['table-wrapper']}>Countries
-  <div>
-    <button onClick={changeSelector}>&lt;</button> {nameSelector}<button id='next' onClick={changeSelector}>&gt;</button>
-  </div>
+const changeFindInput = (e) => {
+  Setvalue(e.currentTarget.value)
+}
 
+if (value.length !== 0) {
+  sum = sum.filter((el) => el.Country.startsWith(value))
+}
+
+// console.log(population[0].population)
+
+ if(numSelector <= 5) {
+  sum.sort((a,b) => b[nameSelector] - a[nameSelector]);
+ } else {
+  sum.sort((a,b) => b[selectors[numSelector % 6]] - a[selectors[numSelector % 6]]);
+  // sum.sort((a,b) => (b[nameSelector] / (population.find(item => item.name == b.Country)).population) * 100000 -
+  //  (a[nameSelector] / (population.find(item => item.name == a.Country)).population) * 100000);
+
+  return (
+    <div className={styles['table-wrapper']}><span>Countries</span>
+  <div className={styles['containerSelectors']}>
+    <button onClick={changeSelector}>&lt;</button> <span className={styles['selectors']}>{nameSelector}</span><button id='next' onClick={changeSelector}>&gt;</button>
+  </div>
+  <div><span>Find:</span><input className={styles['findArea']} onChange={changeFindInput} type="text" value={value}/></div>
   <div className={styles['container']}>
     <div>
-    {sum.map((el, id) => <div key={id}> <img src={`https://www.countryflags.io/${el.CountryCode.toLowerCase()}/flat/16.png`}></img> {el.Country} </div>)}
+    {sum.map((el, id) => <div key={id}> <img className={styles['flagImg']} src={`https://www.countryflags.io/${el.CountryCode.toLowerCase()}/flat/16.png`}></img>
+     <span className={styles['countries']}>{el.Country}</span><span className={styles['countriesValue']}>{el[selectors[numSelector % 6]]}</span> </div>)}
     </div>
+  </div>
+</div>
+  );
+ }
+
+return (
+<div className={styles['table-wrapper']}> <span>Countries</span>
+  <div className={styles['containerSelectors']}>
+    <button onClick={changeSelector}>&lt;</button> <span className={styles['selectors']}>{nameSelector}</span><button id='next' onClick={changeSelector}>&gt;</button>
+  </div>
+  <div><span>Find:</span><input className={styles['findArea']} onChange={changeFindInput} type="text" value={value}/></div>
+  <div className={styles['container']}>
     <div>
-    {sum.map((el, id) => <div key={id}> {el.TotalConfirmed} </div>)}
+    {sum.map((el, id) => <div key={id}> <img className={styles['flagImg']}src={`https://www.countryflags.io/${el.CountryCode.toLowerCase()}/flat/16.png`}></img>
+    <span className={styles['countries']}>{el.Country}</span><span className={styles['countriesValue']}>{el[nameSelector]}</span> </div>)}
     </div>
   </div>
 </div>
 );
-
-
-
 
 }
 
