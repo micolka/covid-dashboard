38fe5f59e2f014e7861a258f4a00649bc9886f3e
Александра Садовская <43875392+SashaSadovskaya@users.noreply.github.com>
2020-12-17 21:58:42 +0300

    Feat/stats add switch (#11)
    * feat: change eslintrc, package.json
    
    * feat: add global deaths
    
    * feat: added a toggle switch for global data and one day data
    
    * feat: added a toggle switch for global data and one day data
    
    * feat: add constants
    
    * feat: fix preloader after merge
    
    * feat: fix package-json.loc
    
    * fix: delete preloader

---
+++ src/components/Switch/Switch.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styles from '@/assets/stylesheets/switch.scss';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const ToggleSwitch = ({ checked, onChange }) => (
+  <div className={styles['toggle-switch']}>
+    <input
+      type="checkbox"
+      className={styles['toggle-switch-checkbox']}
+      name="toggleSwitch"
+      id="toggleSwitch"
+      onChange={e => onChange(e.target.checked)}
+    />
+    {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+    <label className={styles['toggle-switch-label']} htmlFor="toggleSwitch">
+      <span className={styles['toggle-switch-inner']} />
+      <span className={styles['toggle-switch-switch']} />
+    </label>
+  </div>
+);
+
+export default ToggleSwitch;
