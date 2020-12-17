21d2b168a093be515ba5a6c787e365cab9a5e1df
asadovskaya <sashasadovskaya@gmail.com>
2020-12-17 16:46:13 +0300

    feat: added a toggle switch for global data and one day data

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,30 +1,58 @@
 import React, { useEffect, useState } from 'react';
 
-import { getSummary } from '@/API/API';
 import styles from '@/assets/stylesheets/stats.scss';
+import ToggleSwitch from '@/components/Switch/Switch';
 
 const Stats = props => {
-  const [data, setData] = useState({});
-  const [countries, setCountries] = useState([]);
+  const [checked, setChecked] = useState(false);
+  const { summary } = props;
+  const [isFormatted, setState] = useState(false);
+  const [totalConfirmed, setTotalConfirmed] = useState(summary.Global.TotalConfirmed);
+  const [totalDeaths, setTotalDeaths] = useState(summary.Global.TotalDeaths);
+  const [totalRecovered, setTotalRecovered] = useState(summary.Global.TotalRecovered);
+  const [newConfirmed, setNewConfirmed] = useState(summary.Global.NewConfirmed);
+  const [newDeaths, setNewDeaths] = useState(summary.Global.NewDeaths);
+  const [newRecovered, setNewRecovered] = useState(summary.Global.NewRecovered);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getSummary();
-      const countries = await result.Countries[0].Country;
-      const countriesDeaths = await result.Countries[0].TotalDeaths;
-      setData(result);
-      setCountries(countries);
-    };
+  function recalculate() {
+    setTotalConfirmed(Math.round(totalConfirmed / 7827));
+    setTotalDeaths(Math.round(totalDeaths / 7827));
+    setTotalRecovered(Math.round(totalRecovered / 7827));
+    setNewConfirmed(Math.round(newConfirmed / 7827));
+    setNewDeaths(Math.round(newDeaths / 7827));
+    setNewRecovered(Math.round(newRecovered / 7827));
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    if (isFormatted === false)isFormatted.setState(true);
+  }
 
-    fetchData();
-  }, []);
+  const confirmed = checked ? totalConfirmed : newConfirmed;
+  const deaths = checked ? totalDeaths : newDeaths;
+  const recovered = checked ? totalRecovered : newRecovered;
+  const title = checked ? 'For all the time' : 'For one day';
 
   return (
     <div className={styles['stats-wrapper']}>
-      <h2>Global Deaths</h2>
-      <p>
-        {data.Global ? data.Global.TotalDeaths : ''}
-      </p>
+      <h2>Global</h2>
+      <ToggleSwitch id="toggleSwitch" checked={checked} onChange={setChecked} />
+      <div className={styles['stats-table']}>
+        <div className={styles['stats-global-total-date']}>
+          <h3>{title}</h3>
+          <h4>Confirmed</h4>
+          <p>
+            {confirmed}
+          </p>
+          <h4>Deaths</h4>
+          <p>
+            {deaths}
+          </p>
+          <h4>Recovered</h4>
+          <p>
+            {recovered}
+          </p>
+        </div>
+      </div>
+      {/* eslint-disable-next-line react/button-has-type */}
+      <button onClick={recalculate} className={styles['button-recalculate']}>per 100th population</button>
     </div>
   );
 };
