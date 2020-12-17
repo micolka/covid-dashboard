03d474206f2e26c44a2d3d3721dff9d29b738237
Vladimir <66167645+Vladimir0087@users.noreply.github.com>
2020-12-17 23:26:07 +0300

    fix: Table functionality and prev mistakes (#12)

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,78 +1,76 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
-import { selectors } from './Constants';
 import styles from '@/assets/stylesheets/table.scss';
 
-const Table = (props) => {
-const [numSelector, setNumSelector] = useState(0);
-const [value, Setvalue] = useState('');
+import { selectors } from './Constants';
 
-let sum = props.summary.Countries;
-const population = props.summary.population;
-// console.log(population)
- const nameSelector = selectors[numSelector];
+const Table = props => {
+  const [numSelector, setNumSelector] = useState(0);
+  const [value, setValue] = useState('');
 
-// console.log(sum)
+  let sum = props.summary.Countries;
+  const nameSelector = selectors[numSelector];
+  const per100K = 100000;
 
- const changeSelector = (e) => {
-   if (e.target.closest('button').id === 'next') {
-     return setNumSelector (prev => (prev + 1) % 12)
-   } else {
-     return setNumSelector (prev => {
-       if(prev <= 0) return 12 - Math.abs((prev - 1)) % 12;
-       return (prev - 1) % 12;
-      })
-   }
- }
+  // console.log(sum)
 
-const changeFindInput = (e) => {
-  Setvalue(e.currentTarget.value)
-}
+  const changeSelector = e => {
+    if (e.target.closest('button').id === 'next') {
+      return setNumSelector(prev => (prev + 1) % 12);
+    }
+    return setNumSelector(prev => {
+      if (prev === 0) return 11;
+      return (prev - 1) % 12;
+    });
+  };
 
-if (value.length !== 0) {
-  sum = sum.filter((el) => el.Country.startsWith(value))
-}
+  const changeFindInput = e => {
+    setValue(e.currentTarget.value);
+  };
 
-// console.log(population[0].population)
+  if (value.length !== 0) {
+    sum = sum.filter(el => el.Country.toUpperCase().startsWith(value.toUpperCase()));
+  }
 
- if(numSelector <= 5) {
-  sum.sort((a,b) => b[nameSelector] - a[nameSelector]);
- } else {
-  sum.sort((a,b) => b[selectors[numSelector % 6]] - a[selectors[numSelector % 6]]);
-  // sum.sort((a,b) => (b[nameSelector] / (population.find(item => item.name == b.Country)).population) * 100000 -
-  //  (a[nameSelector] / (population.find(item => item.name == a.Country)).population) * 100000);
+  if (numSelector <= 5) {
+    sum.sort((a, b) => b[nameSelector] - a[nameSelector]);
+  } else {
+    sum.sort((a, b) => (b[selectors[numSelector % 6]] / (b.population * per100K))
+    - (a[selectors[numSelector % 6]] / (a.population * per100K)));
+  }
+
+  const selectedCountry = e => {
+    const countryName = e.target.innerHTML;
+    return sum.find(el => el.Country === countryName);
+  };
 
   return (
-    <div className={styles['table-wrapper']}><span>Countries</span>
-  <div className={styles['containerSelectors']}>
-    <button onClick={changeSelector}>&lt;</button> <span className={styles['selectors']}>{nameSelector}</span><button id='next' onClick={changeSelector}>&gt;</button>
-  </div>
-  <div><span>Find:</span><input className={styles['findArea']} onChange={changeFindInput} type="text" value={value}/></div>
-  <div className={styles['container']}>
-    <div>
-    {sum.map((el, id) => <div key={id}> <img className={styles['flagImg']} src={`https://www.countryflags.io/${el.CountryCode.toLowerCase()}/flat/16.png`}></img>
-     <span className={styles['countries']}>{el.Country}</span><span className={styles['countriesValue']}>{el[selectors[numSelector % 6]]}</span> </div>)}
+    <div className={styles['table-wrapper']}>
+      <span>Countries</span>
+      <div className={styles.containerSelectors}>
+        <button type="button" onClick={changeSelector}>&lt;</button>
+        <span className={styles.selectors}>{nameSelector}</span>
+        <button type="button" id="next" onClick={changeSelector}>&gt;</button>
+      </div>
+      <div>
+        <span>Find:</span>
+        <input className={styles.findArea} onChange={changeFindInput} type="text" value={value} />
+      </div>
+      <div className={styles.container}>
+        <div>
+          {sum.map(el => (
+            <div key={el.CountryCode}>
+              <img className={styles.flagImg} alt="flag" src={`https://www.countryflags.io/${el.CountryCode.toLowerCase()}/flat/16.png`} />
+              <span onClick={selectedCountry} className={styles.countries}>{el.Country}</span>
+              {numSelector <= 5
+                ? <span className={styles.countriesValue}>{el[nameSelector]}</span>
+                : <span className={styles.countriesValue}>{(el[selectors[numSelector % 6]] / (el.population * per100K)).toFixed(3)}</span>}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
- }
-
-return (
-<div className={styles['table-wrapper']}> <span>Countries</span>
-  <div className={styles['containerSelectors']}>
-    <button onClick={changeSelector}>&lt;</button> <span className={styles['selectors']}>{nameSelector}</span><button id='next' onClick={changeSelector}>&gt;</button>
-  </div>
-  <div><span>Find:</span><input className={styles['findArea']} onChange={changeFindInput} type="text" value={value}/></div>
-  <div className={styles['container']}>
-    <div>
-    {sum.map((el, id) => <div key={id}> <img className={styles['flagImg']}src={`https://www.countryflags.io/${el.CountryCode.toLowerCase()}/flat/16.png`}></img>
-    <span className={styles['countries']}>{el.Country}</span><span className={styles['countriesValue']}>{el[nameSelector]}</span> </div>)}
-    </div>
-  </div>
-</div>
-);
-
-}
+};
 
 export default Table;
