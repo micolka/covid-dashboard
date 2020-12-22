2795dcf275f3c4a5defa1ac7c0f8b3ef8dc28e23
asadovskaya <sashasadovskaya@gmail.com>
2020-12-22 23:16:27 +0300

    feat: add logo for header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import VirusSrc from '@/assets/img/virus.png';
 import styles from '@/assets/stylesheets/header.scss';
 
-const Header = () => <div className={styles['header-wrapper']}>Header</div>;
+const Header = () => (
+  <div className={styles['header-wrapper']}>
+    <h1 className={styles['header-title']}>COVID-19 Dashboard</h1>
+    <img className={styles['header-logo']} alt="virus" src={VirusSrc} />
+  </div>
+);
 
 export default Header;
